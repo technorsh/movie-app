cb580ed549fbf762ea57b2319da56f58741a083f
technorsh <technorsh@gmail.com>
2022-02-24 11:43:14 +0530

    phase-2 branch updated

---
+++ .history/scripts/afterinstall_20220224112053.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/frontend
+sudo npm install
+sudo npm install -g --unsafe-perm=true --allow-root
---
+++ .history/scripts/onstart_20220224112048.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/frontend
+sudo pm2 start npm -- start
